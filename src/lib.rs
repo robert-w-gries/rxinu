e6bd283acf2a0d1b62a4cb9a02ac81ea717b3cd1
Rob Gries <robert.w.gries@gmail.com>
2016-10-16 12:09:51 -0400

    Tidy up vga_buffer code
    * println and print macros
    * cleaning up of code infrastructure
    * modularize the different structures

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,17 @@
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+extern crate volatile;
 
+#[macro_use]
 mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    vga_buffer::print_something()
+    vga_buffer::clear_screen();
+    println!("Hello World{} {}", "!", "3");
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
@@ -20,4 +25,3 @@ pub extern fn rust_main() {
 pub extern "C" fn _Unwind_Resume() -> ! {
         loop {}
 }
-
