8e3e3a9bc0ffc9e019fbf2cf088ac5fd9bf3f7f6
Rob Gries <robert.w.gries@gmail.com>
2016-11-30 18:41:02 -0500

    Initial mem commit

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(unique)]
 #![no_std]
 
+extern crate multiboot2;
 extern crate rlibc;
 extern crate spin;
 extern crate volatile;
@@ -11,16 +12,35 @@ extern crate volatile;
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_information_address: usize) {
     vga_buffer::clear_screen();
-    for i in 0..100 {
-        println!("Hello World{} {}", "!", i);
-    }
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+
+    let elf_sections_tag = boot_info.elf_sections_tag()
+            .expect("Elf-sections tag required");
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+            .min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+            .max().unwrap();
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel_start = {}, kernel_end = {}", kernel_start, kernel_end);
+    println!("multiboot_start = {}, multiboot_end = {}", multiboot_start, multiboot_end);
+
     loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
+
+#[lang = "panic_fmt"]
+extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}
 
 #[allow(non_snake_case)]
 #[no_mangle]
