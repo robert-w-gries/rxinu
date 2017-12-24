233863cde13a15d8a84b76d774177eb041bcd433
too-r <william@huddie.net>
2017-12-24 09:36:45 +0000

    Add basic mode 3 PIT driver

--- src/arch/x86/device.rs
+++ src/arch/x86/device.rs
@@ -1,7 +1,8 @@
-use device::{pic_8259, ps2_controller_8042, uart_16550};
+use device::{pic_8259, ps2_controller_8042, uart_16550, pit};
 
 pub fn init() {
     pic_8259::init();
     uart_16550::init();
     ps2_controller_8042::init();
+    pit::init();
 }
---
+++ src/device/pit.rs
@@ -0,0 +1,23 @@
+use syscall::io::Port;
+use syscall::io::Io;
+use spin::Mutex;
+
+/// Operate in channel 0. Use mode 3, and operate with lobyte/hibyte.
+const PIT_SET: u8 = 0x36;
+static DIVISOR: u16 = 2685;
+
+pub static PIT: Mutex<[Port<u8>; 2]> = Mutex::new(unsafe {[
+    // Command register.
+    Port::new(0x43),
+    // Channel 0.
+    Port::new(0x40),
+]});
+
+pub fn init() {
+    PIT.lock()[0].write(PIT_SET);
+    // Lower 8 bytes.
+    PIT.lock()[1].write((DIVISOR & 0xFF) as u8);
+    PIT.lock()[1].write((DIVISOR >> 8) as u8);
+
+    kprintln!("[ OK ] Programmable Interval Timer");
+}
