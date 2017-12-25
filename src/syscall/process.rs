433478113e940c75e6147013f148702bd0c1c959
William Huddie <william@huddie.net>
2017-12-25 20:47:46 +0000

    disable_interrupts_then can now return any type. Wrap create() call under syscall::create() inside a disable_interrupts_then block

--- src/syscall/process.rs
+++ src/syscall/process.rs
@@ -3,9 +3,13 @@ use scheduling::{DoesScheduling, ProcessId, SCHEDULER};
 
 /// Wrapper around scheduler.create() and ready() that can be called in processes
 pub fn create(new_proc: extern "C" fn(), name: String) -> ProcessId {
-    let pid = SCHEDULER
-        .create(new_proc, name)
-        .expect("Could not create new process!");
-    SCHEDULER.ready(pid.clone());
-    pid
+    use arch::interrupts;
+
+    interrupts::disable_interrupts_then(|| -> ProcessId {
+        let pid = SCHEDULER.
+            .create(new_proc, name)
+            .expect("Could not create new process!");
+        SCHEDULER.ready(pid.clone());
+        pid
+    })
 }
