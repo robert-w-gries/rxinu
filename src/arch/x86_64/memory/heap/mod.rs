30ca1e8d6479b68e06d5d7750309ef6250c71506
Rob Gries <robert.w.gries@gmail.com>
2018-05-25 17:08:58 -0400

    Increase heap space after increasing process stack size

--- src/arch/x86_64/memory/heap/mod.rs
+++ src/arch/x86_64/memory/heap/mod.rs
@@ -7,7 +7,7 @@ use core::ptr::NonNull;
 use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: u64 = 0o_000_001_000_000_0000;
-pub const HEAP_SIZE: u64 = 500 * 1024; // 500 KB
+pub const HEAP_SIZE: u64 = 1024 * 1024; // 1 MB
 
 pub struct HeapAllocator {
     inner: LockedHeap,
