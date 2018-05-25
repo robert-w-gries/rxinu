ad52b40356a27add51863276fedc9d849187f35d
Rob Gries <robert.w.gries@gmail.com>
2018-05-25 02:00:02 -0400

    Set process stack size to 4KB

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -18,8 +18,7 @@ pub trait Scheduling {
 }
 
 const MAX_PROCS: usize = usize::max_value() - 1;
-// TODO: Investigage requirements for size of stack
-const INIT_STK_SIZE: usize = 1024 * 2;
+const INIT_STK_SIZE: usize = 1024 * 4;
 
 lazy_static! {
     pub static ref SCHEDULER: Scheduler = Scheduler::new();
