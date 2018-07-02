d901fdac14008b52122dca252401d8ac53bb2e73
Rob Gries <robert.w.gries@gmail.com>
2018-07-02 15:39:03 -0400

    Strengthen the testing of semaphores

--- src/bin/test-task-ipc.rs
+++ src/bin/test-task-ipc.rs
@@ -22,6 +22,7 @@ use rxinu::task::scheduler::global_sched;
 
 lazy_static! {
     static ref SEM: IrqLock<Semaphore> = IrqLock::new(Semaphore::new(2));
+    static ref COUNT: IrqLock<Semaphore> = IrqLock::new(Semaphore::new(0));
 }
 
 #[cfg(not(test))]
@@ -35,14 +36,19 @@ pub extern "C" fn _start(boot_info_address: usize) -> ! {
 
     let p1 = syscall::create(alloc::String::from("process1"), 100, loop_process).unwrap();
 
-    let _ = syscall::yield_cpu().unwrap();
-
-    // Process 1 should run again
-    SEM.lock().signaln(1).unwrap();
-
     let check_state = |p: ProcessId, s: State| global_sched().get_process(p).unwrap().state() == s;
 
+    // Run Process1
+    let _ = syscall::yield_cpu().unwrap();
+
     assert!(check_state(p1, State::Wait));
+    let sem_count = COUNT.lock().count();
+
+    // Process1 runs again then enters WAIT state again
+    SEM.lock().signal().unwrap();
+
+    assert!(check_state(p1, State::Wait));
+    assert!(sem_count < COUNT.lock().count());
 
     serial_println!("ok");
     unsafe {
@@ -54,6 +60,7 @@ pub extern "C" fn _start(boot_info_address: usize) -> ! {
 
 pub extern "C" fn loop_process() {
     loop {
+        let _ = COUNT.lock().signal().unwrap();
         let _ = SEM.lock().wait().unwrap();
     }
 }
