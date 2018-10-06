a3d7cf630b254776915fd36bf5612a3a5f6e4a01
Rob Gries <robert.w.gries@gmail.com>
2018-10-06 12:49:04 -0400

    Resolve panic_handler warnings

--- src/bin/test-basic-boot.rs
+++ src/bin/test-basic-boot.rs
@@ -1,4 +1,3 @@
-#![feature(panic_handler)] // required for defining the panic handler
 #![feature(const_fn)]
 #![no_std] // don't link the Rust standard library
 #![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
--- src/bin/test-panic.rs
+++ src/bin/test-panic.rs
@@ -1,4 +1,3 @@
-#![feature(panic_handler)]
 #![feature(const_fn)]
 #![no_std]
 #![cfg_attr(not(test), no_main)]
